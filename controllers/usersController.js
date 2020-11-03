f8626cd3ff8d8475e294869b9138b935f17e47af
Tiri <shaymusts@gmail.com>
2020-11-03 15:38:18 -0600

    Modularized dummy db

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,32 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
-
-let users = [
-  {
-    "id": uuidv4(),
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz"
-  },
-  {
-    "id": uuidv4(),
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv"
-  },
-  {
-    "id": uuidv4(),
-    "name": "Clementine Bauch",
-    "username": "Samantha",
-    "email": "Nathan@yesenia.net",
-  },
-
-  {
-    "id": uuidv4(),
-    "name": "Patricia Lebsack",
-    "username": "Karianne",
-    "email": "Julianne.OConner@kory.org"
-  }
-]
+import { users } from '../db.js'
 
 export const getUsers = (req, res) => {
   res.send(users)
